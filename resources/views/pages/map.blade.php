4ab798cf10d566cae30d2f516a34d2df232ce9df
dtphelan <dtphelan@gmail.com>
2019-06-05 19:46:14 -0400

    Update map.blade.php

--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -6,8 +6,8 @@
           <h2 class="section-heading">Directions</h2>
         </div>
         <div class='col-lg-12'>
-          <iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJJXdTZpFq4okRV8YOsX3aNOk&key=AIzaSyA-xalat_ZDM-nHea2tRIdxYXLUItkDgT8" allowfullscreen>
-          </iframe>
+          <iframe width="600" height="450" frameborder="0" style="border:0"
+src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJJXdTZpFq4okRV8YOsX3aNOk&key=AIzaSyA-xalat_ZDM-nHea2tRIdxYXLUItkDgT8" allowfullscreen></iframe>
         </div>
       </div>
   </div>
