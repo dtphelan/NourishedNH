44920cf5fe15346103fbd04fef73efe25dac2245
dtphelan <dtphelan@gmail.com>
2021-01-30 14:00:13 -0500

    Update work.blade.php

--- resources/views/pages/work.blade.php
+++ resources/views/pages/work.blade.php
@@ -8,32 +8,32 @@
                 </div>
             </div>
             <div class="row text-center">
-                <div class="col-md-3">
-                    <span class="fa-stack fa-3x">
+                <div class="col-md-6">
+                    <span class="fa-stack fa-6x">
                         <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-id-card-o fa-stack-1x fa-inverse"></i>
+                        <i class="fa fa-cutlery fa-stack-1x fa-inverse"></i>
                     </span>
                     <h4 class="service-heading">Counseling</h4>
                     <p class="text-muted">The initial assessment typically lasts 90 minutes and involves a thorough review of your current and past medical history, laboratory tests, medications and supplements, family history, lifestyle and physical activity, your current eating preferences, nutrition knowledge and skills, and your wellness goals. With this information, we will then customize a plan for you that will keep all of your lifestyle and wellness goals in mind.</p>
                 </div>
-                <div class="col-md-3">
-                    <span class="fa-stack fa-3x">
+                <div class="col-md-6">
+                    <span class="fa-stack fa-6x">
                         <i class="fa fa-circle fa-stack-2x text-primary"></i>
                         <i class="fa fa-user-md fa-stack-1x fa-inverse"></i>
                     </span>
                     <h4 class="service-heading">Follow Ups</h4>
                     <p class="text-muted">Research shows that people have a higher success rate by continuing to work with a Registered Dietitian throughout the change process. Our 30 to 60 minute follow-ups allow us to fine-tune your individualized program, monitor your progress, and provide you with the motivation and support to meet your goals. Long-term maintenance of your changes is enhanced by discussions at the frequency that is right for you, which can vary from weekly to several months dependent on your confidence level and needs.</p>
                 </div>
-                <div class="col-md-3">
-                    <span class="fa-stack fa-3x">
+                <div class="col-md-6">
+                    <span class="fa-stack fa-6x">
                         <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-cutlery fa-stack-1x fa-inverse"></i>
+                        <i class="fa fa-id-card-o fa-stack-1x fa-inverse"></i>
                     </span>
                     <h4 class="service-heading">Insurance</h4>
                     <p class="text-muted">Did you know that our services are covered by most insurance plans? You may not have to pay for your visit! Currently, Nourished Nutrition Counseling LLC accepts Anthem Blue Cross, United Healthcare, Harvard Pilgrim including Health Plans, Cigna, and Tufts. Want to see what your coverage will allow? Contact Nourished right now!</p>
                 </div>
-                <div class="col-md-3">
-                    <span class="fa-stack fa-3x">
+                <div class="col-md-6">
+                    <span class="fa-stack fa-6x">
                         <i class="fa fa-circle fa-stack-2x text-primary"></i>
                         <i class="fa fa-phone fa-stack-1x fa-inverse"></i>
                     </span>
