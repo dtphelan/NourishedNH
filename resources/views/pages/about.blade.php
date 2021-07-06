b3279e07941e033a5533fe620e860fb840cdc9ba
dtphelan <dtphelan@gmail.com>
2021-07-06 08:06:40 -0400

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -29,10 +29,23 @@
                             </div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
-                                    <h4 class="subheading">Background</h4>
+                                    <h4 class="subheading">Meagan Phelan</h4>
                                 </div>
                                 <div class="timeline-body">
                                     <p class="text-muted">Meagan earned her Bachelor’s Degree with Honors in Dietetics, Nutrition and Food Sciences at the University of Vermont. She then completed her Dietetic Internship at the prestigious National Institutes of Health in Bethesda, MD. Before returning to the Granite State, she worked for several years as an Outpatient Dietitian for Kaiser Permanente in Washington, DC. She opened her private practice in 2017 after seeing the harmful effects of diet culture and wanting a hands-on approach to promoting body positivity and acceptance. She currently lives in the Concord area with her husband, son, and their fur-baby, Ellie. She is an active cook, dog walker, book reader, and traveler.</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="timeline-inverted">
+                            <div class="timeline-image">
+                                <img class="img-circle img-responsive" src="img/about/5.jpg" alt="">
+                            </div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4 class="subheading">Michelle Boucher</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Michelle Boucher RD, LDN is a Registered Dietitian and Certified Intuitive Eating Counselor who specializes in disordered eating with a Health at Every Size approach. Michelle graduated with a Bachelor’s degree in Nutrition and Dietetics at the University of Rhode Island and completed her Dietetic Internship through Wellness Workdays where she concentrated in worksite wellness.  Afterwards, she became a Registered Dietitian through the Academy of Nutrition and Dietetics and a Licensed Dietitian through the state of New Hampshire. Michelle became a Certified Intuitive Eating Counselor under supervision from the Original Intuitive Eating Pro Evelyn Tribole MS, RDN, CEDRD-S. She is currently a member of the Academy of Nutrition and Dietetics, the New Hampshire Academy of Nutrition and Dietetics, the Behavioral Health Nutrition Dietetic Practice Group and the Nutrition Entrepreneurs Dietetic Practice Group.</p>
                                 </div>
                             </div>
                         </li>
